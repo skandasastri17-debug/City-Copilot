7a83042c3ff0188a6565e8d2c3f6e684b40aefa2
Skanda S <skandas@Skandas-MacBook-Air.local>
2026-06-25 23:14:45 -0400

    Remove unused legacy routes

--- app/my-badges/page.tsx
+++
@@ -1,5 +0,0 @@
-import { AppShell, BadgeGrid } from "@/components/worldcup";
-
-export default function MyBadgesPage() {
-  return <AppShell title="My badges" eyebrow="Bragging rights"><BadgeGrid /></AppShell>;
-}
--- app/my-results/page.tsx
+++
@@ -1,5 +0,0 @@
-import { AppShell, DemoScoreBreakdown } from "@/components/worldcup";
-
-export default function MyResultsPage() {
-  return <AppShell title="My results" eyebrow="Scored predictions"><DemoScoreBreakdown /></AppShell>;
-}
